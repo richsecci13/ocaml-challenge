503d202ab2e3844920ad65bf510c4f836f125be4
richsecci13 <r.secci53@studenti.unica.it>
2024-10-11 12:17:06 +0200

    Soluzione Aggiornata NAND

--- 1/nand/soluzioneNAND.ml
+++ 1/nand/soluzioneNAND.ml
@@ -1,1 +1,16 @@
-let nand a b = not (a && b)
+let nand1 (a : bool) (b : bool) : bool 
+= not (a && b)
+
+
+let nand2 (a : bool) (b : bool) : bool
+= if a
+  then (* a è true *)
+   (if b
+    then (* b è true *) false
+    else (* b è false *)true)
+  else true
+
+  let nand3 (a : bool) (b : bool) : bool
+  = match (a, b) with
+  | (true, true) -> false
+  | _ -> true
