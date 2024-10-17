3fc10b7e0be32db02d5343a530e05698071d3f9e
richsecci13 <r.secci53@studenti.unica.it>
2024-10-18 00:33:08 +0200

    Aggiornamento mux

--- 1/mux/mux.ml
+++ 1/mux/mux.ml
@@ -1,12 +1,18 @@
 let mux1 (s0 : bool) (a : bool) (b : bool) : bool =
   (s0 && a) || (not s0 && b)
-  
 
 let mux2 (s0 : bool) (a : bool) (b : bool) : bool =
   if s0 then a
     else b
 
 let mux3 (s0 : bool) (a : bool) (b : bool) : bool =
-  match (s0, a, b) with
-  | (true, _, _) -> a
-  | (false, _, _) -> b
+  match (s0) with
+  | (true) -> a
+  | (false) -> b
+
+let mux4 (s1 : bool) (s0 : bool) ( a0 : bool) (a1 : bool) (a2 : bool) (a3 : bool) : bool =
+  match (s1, s0) with
+  | (false, false) -> a0
+  | (false, true) -> a1
+  | (true, false) -> a2
+  | (true, true) -> a3
